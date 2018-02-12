c2a328f7e5c8dd0df04cf0337586d9ddb96d8897
fxf <601244325@qq.com>
2018-02-12 17:13:40 +0800

    修改后台地址

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  NODE_HOST: '"http://211.159.174.88:32776"'
+  NODE_HOST: '"http://47.75.8.92:32776"'
 })
